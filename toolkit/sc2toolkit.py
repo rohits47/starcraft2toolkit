8e45be7114eb5cfa5fcb090684457d1a806c249a
Rohit Sanbhadti <rohits47@gmail.com>
2019-04-12 20:54:29 -0700

    stubbed initial toolkit file

---
+++ toolkit/sc2toolkit.py
@@ -0,0 +1,33 @@
+"""
+sc2toolkit main class
+"""
+import fire
+
+
+class Starcraft2Toolkit:
+    """
+    Starcraft2Toolkit
+    """
+
+    def __init__(self):
+        pass
+
+
+class ToolkitCLI:
+    """
+    ToolkitCLI
+    """
+
+    def __init__(self):
+        pass
+
+    def run(self):
+        pass
+
+
+def main():
+    fire.Fire(ToolkitCLI)
+
+
+if __name__ == '__main__':
+    main()
