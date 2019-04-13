dfc460ef24317065bb541228b03f7e02c667b3df
Rohit Sanbhadti <rohits47@gmail.com>
2019-04-13 00:09:41 -0700

    added enging plugin stub

---
+++ sc2toolkit/engine_toolkit_plugin.py
@@ -0,0 +1,33 @@
+"""
+engine_toolkit_plugin
+"""
+
+
+class EngineToolkitPlugin:
+    """
+    EngineToolkitPlugin
+    """
+
+    def __init__(self):
+        pass
+
+    def handleEvent(self):
+        pass
+
+    def handleMessageEvent(self):
+        pass
+
+    def handleGameEvent(self):
+        pass
+
+    def handleTrackerEvent(self):
+        pass
+
+    def handlePlayerActionEvent(self):
+        pass
+
+    def handleAbilityEvent(self):
+        pass
+
+    def handleHotkeyEvent(self):
+        pass
