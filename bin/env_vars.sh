62615bd634cfd65f5df09d4404e9514542b458d0
Rohit Sanbhadti <rohits47@gmail.com>
2019-04-12 22:33:12 -0700

    initial toolkit operations

--- bin/env_vars.sh
+++ bin/env_vars.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 export SC2READER_CACHE_DIR="sc2reader_cache"
+export SC2READER_CACHE_MAX_SIZE=100
 
--- sc2toolkit/sc2toolkit.py
+++ sc2toolkit/sc2toolkit.py
@@ -8,11 +8,26 @@ import sc2reader
 
 class Starcraft2Toolkit:
     """
-    Starcraft2Toolkit
+    The main toolkit object acts as the entrypoint for most commonly
+    used operations.
     """
 
-    def __init__(self):
+    def __init__(self, filename):
+        self._filename = filename
+        self._replay_dir = None
+        self.replay = None
+        self.load_level = 4
+
+    def dump_vars(self):
+        """
+        return/print all our instance variables, mostly used for
+        debugging from the cli.
+        """
         pass
+
+    def load(self):
+        self.replay = sc2reader.load_replay(
+            self._filename, load_level=self.load_level)
 
 
 def main():
