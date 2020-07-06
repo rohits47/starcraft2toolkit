95b1de02ee0247ccfb612c65d814be3e2b4eddbe
Rohit Sanbhadti <rohits47@gmail.com>
2020-07-05 21:19:08 -0700

    more cli experiments

--- bin/env_vars.sh
+++ bin/env_vars.sh
@@ -3,3 +3,5 @@
 export SC2READER_CACHE_DIR="sc2reader_cache"
 export SC2READER_CACHE_MAX_SIZE=100
 
+# export DEFAULT_REPLAY_FILE="my_sample_replays/sample_replay_TvP.SC2Replay"
+export DEFAULT_REPLAY_FILE="my_sample_replays/2020-07-05_T_grEEngLade_VS_P_Alucardkk.SC2Replay"
--- bin/sc2reader-cli.sh
+++ bin/sc2reader-cli.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
-export DEFAULT_REPLAY_FILE="my_sample_replays/sample_replay_TvP.SC2Replay"
+export DEFAULT_REPLAY_FILE="my_sample_replays/2020-07-05_T_grEEngLade_VS_P_Alucardkk.SC2Replay"
 
 # python sc2reader/sc2reader/scripts/sc2printer.py --help
 
 # print json of replay details
 python sc2reader/sc2reader/scripts/sc2json.py $DEFAULT_REPLAY_FILE | jq
+# python sc2reader/sc2reader/scripts/sc2replayer.py $DEFAULT_REPLAY_FILE
 
 # print high-level game details
 # python sc2reader/sc2reader/scripts/sc2printer.py $DEFAULT_REPLAY_FILE
