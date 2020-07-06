0d70342aa73857c7a17b25bbe080b46dd3ad0e53
Rohit Sanbhadti <rohits47@gmail.com>
2020-07-05 21:21:29 -0700

    parsing experiments

--- bin/sc2reader-cli.sh
+++ bin/sc2reader-cli.sh
@@ -6,7 +6,9 @@ export DEFAULT_REPLAY_FILE="my_sample_replays/2020-07-05_T_grEEngLade_VS_P_Aluca
 
 # print json of replay details
 python sc2reader/sc2reader/scripts/sc2json.py $DEFAULT_REPLAY_FILE | jq
-# python sc2reader/sc2reader/scripts/sc2replayer.py $DEFAULT_REPLAY_FILE
+
+# print plain text output stream of game events
+python sc2reader/sc2reader/scripts/sc2replayer.py $DEFAULT_REPLAY_FILE
 
 # print high-level game details
 # python sc2reader/sc2reader/scripts/sc2printer.py $DEFAULT_REPLAY_FILE
